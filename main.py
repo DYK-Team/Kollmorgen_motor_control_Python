51a244d05781945d8fe5a33d58be5e5f35d19437
Konstantin Gorbatov <65719599+KG1386@users.noreply.github.com>
2022-08-06 13:14:38 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,12 +6,12 @@ p = subprocess.Popen(['run_controller.cmd'])
 time.sleep(5)
 
 def Axis1SetZero():
-    payload = {'Axis1SetZero': True}
+    payload = {'Axis1SetZero': False}
     SetZero1 = requests.put('http://192.168.0.105/kas/plcvariables?format=text',data=payload)
     return SetZero1.status_code
 
 def Axis2SetZero():
-    payload = {'Axis2SetZero': True}
+    payload = {'Axis2SetZero': False}
     SetZero2 = requests.put('http://192.168.0.105/kas/plcvariables?format=text',data=payload)
     return SetZero2.status_code
 
@@ -20,14 +20,25 @@ def ReadActualPosition12():
     return ActualPos12.text  # two values separated with comma
 
 # Returns only status codes; 200 - normal
-def EnableMotors():
-    payload = "OpenButton=False"
-    EnableMotorsS1 = requests.put('http://192.168.0.105/kas/plcvariables?format=text', data=payload)
-    payload = "CloseButton=True"
-    EnableMotorsS2 = requests.put('http://192.168.0.105/kas/plcvariables?format=text', data=payload)
-    Stat1Enabled = requests.get('http://192.168.0.105/kas/plcvariables?variables=Stat1Enabled&format=text')
-    Stat2Enabled = requests.get('http://192.168.0.105/kas/plcvariables?variables=Stat2Enabled&format=text')
-    return EnableMotorsS1.status_code, EnableMotorsS2.status_code, Stat1Enabled.text, Stat2Enabled.text
+def EnableMotors(retry_num = 3):
+    retry_attempts = 0
+    try:
+        payload = "OpenButton=False"
+        EnableMotorsS1 = requests.put('http://192.168.0.105/kas/plcvariables?format=text', data=payload)
+        payload = "CloseButton=True"
+        EnableMotorsS2 = requests.put('http://192.168.0.105/kas/plcvariables?format=text', data=payload)
+        Stat1Enabled = requests.get('http://192.168.0.105/kas/plcvariables?variables=Stat1Enabled&format=text')
+        Stat2Enabled = requests.get('http://192.168.0.105/kas/plcvariables?variables=Stat2Enabled&format=text')
+        return EnableMotorsS1.status_code, EnableMotorsS2.status_code, Stat1Enabled.text, Stat2Enabled.text
+    except:
+        print("trying again")
+        if (retry_attempts <= retry_num):
+            time.sleep(retry_attempts)
+            retry_attempts += 1
+            EnableMotors()
+        else:
+            print ("Failed to enable motors")
+            return False
 
 # Returns only status codes; 200 - normal
 def DisableMotors():
@@ -49,9 +60,6 @@ def checkMotorReadiness2():
     #print(Stat2Enabled.text)
     return Stat2Enabled.text  # staus code; 200 - normal
 
-angle = 360.0  # dgrees
-velocity = 150.0  # degree/second
-acceleration = 900.0  # degree/second^2
 # Change Axis1 parameters
 def Axis1Parameters(angle, velocity, acceleration):
     payload1 = {'Axis1RelDistance': angle}
@@ -111,15 +119,41 @@ def runMotors(axis):
         DriveMotors = requests.put('http://192.168.0.105/kas/plcvariables?format=text', data=payload)
     return DriveMotors.status_code
 
-print(ReadActualPosition12())
+def positionWaiter(timeout, threshold, rel_motion, prev_angle):
+    # for i in range(0,10):
+    print('control = ', float((ReadActualPosition12()).split(",")[0]))
+    print ("outcome", ((abs(float((ReadActualPosition12()).split(",")[0])) - (prev_angle + rel_motion))))
+    while ((abs(float((ReadActualPosition12()).split(",")[0]) - (prev_angle + rel_motion))) > threshold):
+        actual_angle = ReadActualPosition12()
+        time.sleep(timeout)
+        print(actual_angle)
 
-a = Axis1SetZero()
-b = Axis2SetZero()
-time.sleep(1)
+    print("match!")
+    return True
 
-print(Axis2Parameters(angle, velocity, acceleration))
-print(Axis1Parameters(angle, velocity, acceleration))
-a = EnableMotors()
+# Motor tests
+EnableMotors()
 time.sleep(2)
+#print(Axis1SetZero())
+#print(Axis2SetZero())
+#time.sleep(1)
+#target = 0.0
+#positionWaiter(0.1, 0.5, target, achieved)
+#target = achieved
+
+or_reading = ReadActualPosition12()
+print(or_reading)
+prev_angle = float((or_reading).split(",")[0])
+print(prev_angle)
+
+angle1 = -360.0  # dgrees
+velocity = 150.0  # degree/second
+acceleration = 900.0  # degree/second^2
+#print(Axis2Parameters(angle2, velocity, acceleration))
+print(Axis1Parameters(angle1, velocity, acceleration))
+
 print(runMotors(1))
-print(runMotors(2))
+#print(runMotors(2))
+#time.sleep(5)
+positionWaiter(0.1, 0.5, angle1, prev_angle)
+
